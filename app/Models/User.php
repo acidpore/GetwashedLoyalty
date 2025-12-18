9340a11be82c0b86515900dd7c7f99125c4ef580
acidpore <acicitforezt@gmail.com>
2025-12-18 12:36:58 +0700

    update production

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,13 +2,14 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
@@ -57,9 +58,11 @@ class User extends Authenticatable
         return $this->hasOne(Customer::class);
     }
 
-    /**
-     * Check if user is an admin.
-     */
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return $this->role === 'admin';
+    }
+
     public function isAdmin(): bool
     {
         return $this->role === 'admin';
